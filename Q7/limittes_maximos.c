ab1c86bd130ff968cbfad52c41632d1c99484d60
prince nyarko <nyankonton122@gmail.com>
2022-10-09 12:19:36 -0300

    Em ambiente linux os limites superiores da quantidade de processos e threads que podem ser disparados são configuráveis

---
+++ Q7/limittes_maximos.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+ 
+void *thread (void *c){ 
+    // Threads
+}
+  
+int main()
+{
+    int erro = 0, contador = 0, pid, pidmax = 0;
+        pthread_t t;
+    
+    // Criar threads
+    while (erro == 0){
+        erro = pthread_create (&t, NULL, thread, NULL);
+        contador++;
+    }
+    printf("Máximo de threads: %d\n", contador);
+ 
+    // Criar processos      
+    while (1) {
+        pid = fork();
+        pidmax = getpid();
+        if(pid == 0){
+            if(pidmax < getpid())
+            pidmax = getpid();
+            printf("Processos criados: %d\n", pidmax);  
+            return 0;
+        }
+        if(pid == -1) {
+            printf("Máximo de processos: %d\n", pidmax);
+            exit(-1);
+        }
+    }
+}
