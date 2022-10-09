25f50947dbdb532aa5e458e3913858a0a3d280d6
prince nyarko <nyankonton122@gmail.com>
2022-10-08 22:33:21 -0300

    thread ABC

---
+++ thread/thread.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+char *abc[3];
+
+// Função para printar na tela ABC
+void *task_thread(void *tarefa){
+    sleep(1);
+    long Id_do_tarefa;
+    Id_do_tarefa = (long)tarefa;
+    printf("%s", abc[Id_do_tarefa]);
+    // Fazer a thread repetir a mesma ordem
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t threads[3];
+    long tarefa[3];
+    abc[0] = "A"; // Mensagem 1
+    abc[1] = "B"; // Mensagem 2
+    abc[2] = "C"; // Mensagem 1
+
+    for(int i = 0; i < 3; i++){
+        tarefa[i] = i;
+        // Criação de threads com pthread_create
+        pthread_create(&threads[i], NULL, task_thread, (void *) tarefa[i]); 
+        // Fazer a thread repetir a mesma ordem
+        pthread_join(threads[i], NULL);
+    }
+    pthread_exit(NULL); // Fazer a thread repetir a mesma ordem
+    return (0);
+}
