205fee0c89cd49e54c7b0ac6e7c3dea600db174b
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 16:23:40 -0300

    Update pai_2filhos.c

--- Q2/pai_2filhos.c
+++ Q2/pai_2filhos.c
@@ -5,17 +5,26 @@
 #include <unistd.h>
 
 int main() {
+  // Criando Filho1
   pid_t pid = fork();
-
+  // Verificação se o Filho1 foi criado
   if (pid == 0) {
     printf("Filho1 pid %d do Pai pid %d\n", getpid(), getppid());
   } 
+  // Se o Filho1 não foi criado
   else if(pid == -1) {
-    printf("Error em criação filho");
+    printf("Erro na criação do Filho1");
+    return (1);
   }
   else{
+    // Criação de Filho2
     if (fork() == 0) {
       printf("Filho2 pid %d do Pai pid %d\n", getpid(), getppid());
+    }
+    else {
+    // Se o Filho2 não foi criado
+    printf("Erro na criação do Filho2");
+    return (2);
     }
   }
   return (0);
