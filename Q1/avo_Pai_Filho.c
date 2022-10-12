951aa9b7e47d5e4eac79001d7c5c73906b159129
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 16:17:51 -0300

    Update avo_Pai_Filho.c

--- Q1/avo_Pai_Filho.c
+++ Q1/avo_Pai_Filho.c
@@ -8,20 +8,20 @@
 
 int main() {
 
-    // Creiando novo processo Pai do Avô
+    // Criando novo processo Pai do Avô
     if (fork() == 0) {
 
-        if (fork() == 0) { // Creiando novo processo Filho do Pai
+        if (fork() == 0) { // Criando novo processo Filho do Pai
             printf("Em Processo do Filho: %d, o Pai : %d\n",getpid(), getppid());
         }
         
         else {
-          wait(0);  // Aguarda a saída do processo filho
+          wait(0);  // Aguarda a saída do processo Filho
           printf("Em Processo do Pai: %d, o Filho do Avô : %d\n",getpid(), getppid());
         }
 
     } else {
-        wait(0); // Aguarda saída do processo pai
+        wait(0); // Aguarda saída do processo Pai
         printf("Processo Avô: %d e o Pai do Avô: %d \n", getpid(),getppid());
     }
     
