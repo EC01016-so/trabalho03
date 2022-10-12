d90955f6eb0611013eff4de74f2807add0adab02
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 16:29:51 -0300

    Update elimine_o_pai.c

--- Q3/elimine_o_pai.c
+++ Q3/elimine_o_pai.c
@@ -12,26 +12,30 @@ int main() {
 
     pid_do_pai = getpid();
 
-    // Creiando novo processo Pai do Avô
+    // Criando novo processo Pai do Avô
     if (fork() == 0) {
-        wait(0);  // Aguarda a saída do processo filho
-        printf("Em Processo do Pai: %d, o Filho do Avô : %d\n",getpid(), getppid());
+        printf("No processo do Pai: %d , o Filho do Avô: %d\n",getpid(), getppid());
         
-        if (fork() == 0) { // Creiando novo processo Filho do Pai
-            printf("Em Processo do Filho: %d, o Filho do Pai : %d\n",getpid(), getppid());
-
+        // Criando novo processo Filho do Pai
+        if (fork() == 0) {
+            
+            printf("No processo do Filho: %d , o Filho do Pai: %d\n",getpid(), getppid());
             pid_do_pai = getppid();
-            int elimina_o_pai = kill(pid_do_pai, 9); // Eliminando processo pai com sig 9 que é para matar o Pai
-
+            
+            // Eliminando processo pai com sinal 9, que é para matar o Pai
+            int elimina_o_pai = kill(pid_do_pai, 9);
+            
+            // Verifica se o Pai foi eliminado
             if (elimina_o_pai == -1) {
-                printf("error na eliminação do Pai");
+                printf("Erro na eliminação do Pai");
             }
-            printf("Apos elimina o Pai: %d agore o Pai do Filho: %d é %d\n",pid_do_pai, getpid(), getppid());
+            printf("Após eliminar o Pai: %d , agora o Pai do Filho: %d é %d\n",pid_do_pai, getpid(), getppid());
         }
 
     } else {
-        wait(0); // Aguarda saída do processo pai
-        printf("Processo Avô: %d o Pai do Avô: %d\n", getpid(),getppid());
+        // Aguarda saída do processo Filho
+        wait(0);
+        printf("No processo Avô: %d , o Pai do Avô: %d\n", getpid(),getppid());
     }
     
     return (0);
